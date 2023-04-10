9d3acc728bf34d8580be647d418a0aad10805bee
parsajiravand <parsajiravand@gmail.com>
2023-04-10 12:05:05 +0330

    fix v-for in i18n and typee format

--- components/CvContent.vue
+++ components/CvContent.vue
@@ -1,25 +1,29 @@
 <template>
   <div>
     <div class="py-5">
-      <h4 class="font-normal text-2xl mb-4 text-blue-800 dark:text-white ">
-        {{ $t('home.aboutMe.title') }}
+      <h4 class="font-normal text-2xl mb-4 text-blue-800 dark:text-white">
+        {{ t("home.aboutMe.title") }}
       </h4>
       <p class="leading-relaxed">
-        {{ $t('home.aboutMe.description') }}
+        {{ t("home.aboutMe.description") }}
       </p>
     </div>
-    <div class=" py-5 mt-5">
-      <h4 class="font-normal text-2xl mb-8 text-blue-800 dark:text-white ">
-        {{ $t('home.experience.title') }}
+    <div class="py-5 mt-5">
+      <h4 class="font-normal text-2xl mb-8 text-blue-800 dark:text-white">
+        {{ t("home.experience.title") }}
       </h4>
-      <div v-for="(item,index) in $t('home.experience.items')" :key="index" class="mb-5 border p-5 rounded-2xl">
-        <p class="font-extrabold text-lg  text-blue-800 dark:text-white ">
+      <div
+        v-for="(item,index) in tm('home.experience.items' as string)"
+        :key="index"
+        class="mb-5 border p-5 rounded-2xl"
+      >
+        <p class="font-extrabold text-lg text-blue-800 dark:text-white">
           {{ item.title }}
         </p>
-        <p class="font-bold text-md  text-blue-700 dark:text-white ">
+        <p class="font-bold text-md text-blue-700 dark:text-white">
           {{ item.job }}
         </p>
-        <p class="text-light mb-2 text-blue-700 dark:text-white ">
+        <p class="text-light mb-2 text-blue-700 dark:text-white">
           {{ item.time }}
         </p>
         <p>
@@ -30,15 +34,19 @@
         </p>
       </div>
     </div>
-    <div class=" py-5 mt-5">
-      <h4 class="font-normal text-2xl mb-4 text-blue-800 dark:text-white ">
-        {{ $t('home.ecucation.title') }}
+    <div class="py-5 mt-5">
+      <h4 class="font-normal text-2xl mb-4 text-blue-800 dark:text-white">
+        {{ t("home.ecucation.title") }}
       </h4>
-      <div v-for="(item,index) in $t('home.ecucation.items')" :key="index" class="mt-8">
-        <p class="font-bold text-lg  text-blue-800 dark:text-white ">
+      <div
+        v-for="(item,index) in tm('home.ecucation.items' as string)"
+        :key="index"
+        class="mt-8"
+      >
+        <p class="font-bold text-lg text-blue-800 dark:text-white">
           {{ item.title }}
         </p>
-        <p class="font-normal text-md mb-2 text-blue-700 dark:text-white ">
+        <p class="font-normal text-md mb-2 text-blue-700 dark:text-white">
           {{ item.time }}
         </p>
         <p>
@@ -48,8 +56,6 @@
     </div>
   </div>
 </template>
-<script>
-export default {
-  name: 'CvContent'
-}
+<script setup lang="ts">
+const { tm, t } = useI18n();
 </script>
