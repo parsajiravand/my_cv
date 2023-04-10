fb97d35427c17ad67f3acd108573a4c3dea16f2f
parsajiravand <parsajiravand@gmail.com>
2023-04-10 16:10:45 +0330

    change title tag

--- components/ProfileBox.vue
+++ components/ProfileBox.vue
@@ -7,14 +7,14 @@
         alt="parsa jiravand"
       />
     </a>
-    <h2 class="font-bold text-4xl text-blue-800 dark:text-white my-4">
+    <h1 class="font-bold text-4xl text-blue-800 dark:text-white my-4">
       {{ t("home.firstName") }}
 
       {{ t("home.lastName") }}
-    </h2>
-    <h4 class="font-weight-light text-2xl">
+    </h1>
+    <h2 class="font-weight-light text-2xl">
       {{ t("home.career") }}
-    </h4>
+    </h2>
     <section>
       <h4 class="font-weight-light text-2xl" />
     </section>
